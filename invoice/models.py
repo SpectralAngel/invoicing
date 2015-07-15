bbba09c7b5c29abd249b0b877bc50b593dcfddab
Carlos Flores <cafg10@gmail.com>
2015-07-15 13:52:17 -0600

    Remove unrelated parts

--- invoice/models.py
+++ invoice/models.py
@@ -16,6 +16,7 @@ class Invoice(TimeStampedModel):
     number = models.IntegerField(default=0)
     place = models.ForeignKey(Place)
     account = models.ForeignKey(Account)
+    closed = models.BooleanField(default=False)
 
     def __str__(self):
         return u'{0} {1}'.format(self.numero, self.client)
@@ -27,9 +28,6 @@ class Invoice(TimeStampedModel):
             self.number = place.next_receipt_number
             place.next_receipt_number = F('next_receipt_number') + 1
             place.save()
-
-        if self.ciudad is None and self.pk is not None:
-            self.ciudad = self.cajero.profile.ciudad
 
         super(Invoice, self).save(*args, **kwargs)
 
