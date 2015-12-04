ce157bc0e1d6320c5469e68efa9aa9c93e8fb059
Carlos Flores <cafg10@gmail.com>
2015-12-04 11:47:04 -0600

    Revert "Lets simply agree in this file name"
    This reverts commit 6da9ce9cc5be845b26083aa6de852b7d3a690827.

--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -13,7 +13,7 @@ module.exports = {
     },
 
     plugins: [
-        new BundleTracker({filename: './webpack-stats.json'}),
+        new BundleTracker({filename: './webpack-stats-prod.json'}),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
